2420c80b2ab3314cb22658c9b64a78d9ea48f000
lochinbek9 <lochinbeksetor@gmail.com>
2025-08-13 22:05:07 +0500

    navbar responsive

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,48 @@
-import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
+import { Button, Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle, TextInput } from "flowbite-react";
 import {Link} from "react-router-dom"
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
 
 function Header() {
   return (
          <Navbar className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
             <Link to="/">
-                <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">LODE-X'</span>
+                <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">LODE-X'</span>
                 BLOG
             </Link>
+            <form className="flex gap-2">
+                <TextInput type="text" placeholder="Search..." rightIcon={AiOutlineSearch} className="hidden lg:inline"/>
+            </form>
+                <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+                    <AiOutlineSearch/>
+                </Button>
+                <div className="flex gap-2 md:order-2">
+                    <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+                        <FaMoon/>
+                    </Button>
+                    <Link to="/sign-in">
+                        <Button pill> Sign In</Button>
+                    </Link>
+                    <NavbarToggle/>
+                </div>
+        
+            <NavbarCollapse>
+                        <NavbarLink active>
+                            <Link to="/">
+                                Home
+                            </Link>
+                        </NavbarLink>
+                        <NavbarLink>
+                            <Link to="/about">
+                               About
+                            </Link>
+                        </NavbarLink>
+                        <NavbarLink>
+                            <Link to="/projects">
+                              Projects
+                            </Link>
+                        </NavbarLink>
+                    </NavbarCollapse>
          </Navbar>
   )
 }
