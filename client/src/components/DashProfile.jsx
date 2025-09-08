c1ef79d9ef271c2613fa4dfef4c37f5d1c48cd11
lochinbek9 <lochinbeksetor@gmail.com>
2025-09-08 16:20:08 +0500

    Complate profile page UI

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,8 +1,30 @@
 
+import { useSelector } from "react-redux"
+import {TextInput, Button} from "flowbite-react"
+
+
 function DashProfile() {
-  return (
-    <div>DashProfile</div>
-  )
+    const { currentUser } = useSelector((state) => state.user)
+    return (
+        <div className="max-w-lg mx-auto p-3 w-full">
+            <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
+            <form className="flex flex-col gap-4">
+                <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+                    <img src={currentUser.profilePicture} alt="" width="80" height="80" className="rounded-full object-cover w-full h-full border-8 border-[lightgray]" />
+                </div>
+                <TextInput type="text" id="username" placeholder="username" defaultValue={currentUser.username}/>
+                <TextInput type="text" id="email" placeholder="email" defaultValue={currentUser.useremail}/>
+                <TextInput type="password" id="password" placeholder="**********" defaultValue={currentUser.userpassword}/>
+                <Button type="submit" className="bg-blue-600 outline hover:bg-blue-700">
+                    Update
+                </Button>
+            </form>
+            <div className="mt-10 text-red-600 text-center cursor-pointer  flex justify-between gap-3">
+                <a href="#" className="hover:underline">Delete Account</a>
+            <a href="#" className="hover:underline">Sign Out</a>
+            </div>
+        </div>
+    )
 }
 
 export default DashProfile
--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -4,26 +4,26 @@ import { Link, useLocation } from "react-router-dom";
 
 export default function DashSidebar() {
     const location = useLocation();
-  const [tab, setTab] = useState();
+    const [tab, setTab] = useState();
 
-  useEffect(() =>{
-    const urlParams = new URLSearchParams(location.search);
-    const tabFromUlr = urlParams.get("tab");
-    if(tabFromUlr){
-      setTab(tabFromUlr);
-    }
-  }, [location]);
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const tabFromUlr = urlParams.get("tab");
+        if (tabFromUlr) {
+            setTab(tabFromUlr);
+        }
+    }, [location]);
     return (
         <Sidebar className="w-full md:w-56 h-screen" aria-label="Sidebar with multi-level dropdown example">
             <SidebarItems>
                 <SidebarItemGroup>
-                    <Link to="/dashboard?tab=profile" style={{ textDecoration: 'none' }}>
-                    
+                    <Link to="/dashboard?tab=profile"
+                        style={{ textDecoration: 'none' }}>
+                        <SidebarItem active={tab === "profile"} label="User" labelColor="dark">
+                            Profile
+                        </SidebarItem>
                     </Link>
-                    <SidebarItem active={tab === "profile"} href="#" label="User" labelColor="dark">
-                        Profile
-                    </SidebarItem>
-                    <SidebarItem href="#" label="Users">
+                    <SidebarItem  label="Users">
                         Sign Out
                     </SidebarItem>
                 </SidebarItemGroup>
